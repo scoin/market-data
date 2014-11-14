9ec971634406d3999669b3dcba880496bba96a8b
scoin <greg.piccolo@gmail.com>
2014-11-13 19:51:52 -0500

    working live code

--- markit.py
+++ markit.py
@@ -1,2 +1,21 @@
 import requests
 
+class Markit:
+	def __init__(self):
+		self.symbol_url = "http://dev.markitondemand.com/Api/v2/Lookup/json?input="
+		self.quote_url = "http://dev.markitondemand.com/Api/v2/Quote/json?symbol="
+
+	def get_symbol(self, name):
+		url = self.symbol_url + name
+		symbol_object = requests.get(url).json()
+		return(symbol_object)
+
+	def get_quote(self, symbol):
+		url = self.quote_url + symbol
+		symbol_object = requests.get(url).json()
+		return(symbol_object)
+
+api = Markit()
+
+print(api.get_symbol("Microsoft"))
+print(api.get_quote("MSFT"))
--- scrape.py
+++ scrape.py
@@ -1,3 +1,22 @@
 import requests
 import bs4
-import re
+
+class CoinMarket:
+	def __init__(self):
+		self.site = requests.get("http://www.coinmarketcap.com")
+		self.soup = bs4.BeautifulSoup(self.site.text)
+
+	def get_coins(self):
+		headers = False
+		currencies = {}
+		for element in self.soup.select('#currencies tr'):
+			if(headers == False):
+				headers = True
+				continue
+			coin = element.find_all('a')
+			currencies[coin[0].text] = coin[1].text
+		return currencies
+
+coins = CoinMarket()
+currencies = coins.get_coins()
+print(currencies)
