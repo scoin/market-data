19b64a1285eb1af216fbf6ae6a56e60f9056a81b
scoin <greg.piccolo@gmail.com>
2014-11-24 17:05:37 -0500

    comments

--- markit.py
+++ markit.py
@@ -1,21 +1,23 @@
-import requests
+import requests ##python requests http://docs.python-requests.org/en/latest/
 
+##api wrapper
 class Markit:
 	def __init__(self):
+		##api endpoint urls
 		self.symbol_url = "http://dev.markitondemand.com/Api/v2/Lookup/json?input="
 		self.quote_url = "http://dev.markitondemand.com/Api/v2/Quote/json?symbol="
 
-	def get_symbol(self, name):
-		url = self.symbol_url + name
-		symbol_object = requests.get(url).json()
+	def get_symbol(self, symbol):
+		url = self.symbol_url + symbol ##join the url with the symbol input
+		symbol_object = requests.get(url).json() ##parse json returned from the web
 		return(symbol_object)
 
 	def get_quote(self, symbol):
-		url = self.quote_url + symbol
-		symbol_object = requests.get(url).json()
+		url = self.quote_url + symbol ##join the url with the quote input
+		symbol_object = requests.get(url).json() ##parse json returned from the web
 		return(symbol_object)
 
-api = Markit()
+api = Markit() ##instantiate api wrapper
 
 print(api.get_symbol("Microsoft"))
 print(api.get_quote("MSFT"))
--- scrape.py
+++ scrape.py
@@ -1,20 +1,22 @@
-import requests
-import bs4
+import requests ##python requests http://docs.python-requests.org/en/latest/
+import bs4 ##beautiful soup 4 http://www.crummy.com/software/BeautifulSoup/bs4/doc/
+
+###GO TO http://www.coinmarketcap.com and open up Dev Tools.
 
 class CoinMarket:
 	def __init__(self):
-		self.site = requests.get("http://www.coinmarketcap.com")
-		self.soup = bs4.BeautifulSoup(self.site.text)
+		self.site = requests.get("http://www.coinmarketcap.com") ##download html from site
+		self.soup = bs4.BeautifulSoup(self.site.text) ##parse html
 
 	def get_coins(self):
 		headers = False
 		currencies = {}
-		for element in self.soup.select('#currencies tr'):
-			if(headers == False):
+		for element in self.soup.select('#currencies tr'): ##select all table rows in the currencies div and iterate through
+			if(headers == False): 
 				headers = True
-				continue
-			coin = element.find_all('a')
-			currencies[coin[0].text] = coin[1].text
+				continue ##skip headers row
+			coin = element.find_all('a') ##get an array of a tags
+			currencies[coin[0].text] = float(coin[1]['data-usd']) #key = Coin Name (ie: Bitcoin) value = Price
 		return currencies
 
 coins = CoinMarket()
